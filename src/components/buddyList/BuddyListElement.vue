117d1a042692b724012fdac774a71030efdd29f6
Reimo <reimovellemaa@gmail.com>
2022-04-11 16:42:12 +0200

    replace checkboxes with stars and half-stars in tab2

--- src/components/buddyList/BuddyListElement.vue
+++ src/components/buddyList/BuddyListElement.vue
@@ -2,7 +2,12 @@
   <ion-card>
     <ion-item>
       <ion-label>{{ buddyName }}</ion-label>
-      <ion-checkbox v-for="completeness in weeklyCompleteness" :checked="completeness===1" :key="completeness" slot="end"></ion-checkbox>
+      <ion-icon v-for="completeness in weeklyCompleteness"
+                :icon="completeness === 1 ? star : starHalf"
+                :style="completeness === 1 ? 'color: orange;' : 'color:gray;'"
+                size="small"
+                :key="completeness" slot="end">
+      </ion-icon>
       <!--TODO use ration to make it to a filled circle or smth -->
       <ion-item v-if="+streakInDays > 0">
         <ion-icon :icon="flame" size="large" style="color: orange;"></ion-icon>
@@ -15,7 +20,7 @@
 </template>
 <script>
 
-import {flame} from "ionicons/icons";
+import {flame, star, starHalf} from "ionicons/icons";
 import {IonIcon, IonLabel} from "@ionic/vue";
 
 export default {
@@ -27,6 +32,8 @@ export default {
   setup() {
     return {
       flame,
+      starHalf,
+      star
     }
   }
 
